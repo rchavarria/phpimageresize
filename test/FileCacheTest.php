67a5049ba91f6c86bcedbdfb67cdab9438178931
Rubén Chavarría <txingo@gmail.com>
2016-03-22 09:19:56 +0100

    Remove duplicated code in test

--- test/FileCacheTest.php
+++ test/FileCacheTest.php
@@ -13,24 +13,16 @@ class FileCacheTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testFileIsNotInCacheWhenExistingVersionIsOlderThanOriginalFile() {
-        $existingVersionPath = 'existing-version-path.jpg';
-        $originalVersionPath = 'original-version-path.jpg';
-
-        $fs = $this->getMockBuilder('FileSystem')->getMock();
-        $fs->method('file_exists')->willReturn(true);
-        $fs->method('lastModificationDate')
-            ->withConsecutive(
-                [ $this->equalTo($originalVersionPath) ],
-                [ $this->equalTo($existingVersionPath) ]
-            )
-            ->willReturnOnConsecutiveCalls(10, 5);
-
-        $cache = new FileCache($fs);
-        $isInCache = $cache->isInCache($existingVersionPath, $originalVersionPath);
+        $isInCache = $this->exerciseIsInCache(5, 10);
         $this->assertFalse($isInCache, 'Should not be in cache when existing version is older than the original one');
     }
 
     public function testFileIsAlreadyCached() {
+        $isInCache = $this->exerciseIsInCache(10, 5);
+        $this->assertTrue($isInCache, 'Should be in cache when existing version is newer than the original one');
+    }
+
+    private function exerciseIsInCache($existingVersionAge, $originalVersionAge) {
         $existingVersionPath = 'existing-version-path.jpg';
         $originalVersionPath = 'original-version-path.jpg';
 
@@ -41,11 +33,13 @@ class FileCacheTest extends PHPUnit_Framework_TestCase {
                 [ $this->equalTo($originalVersionPath) ],
                 [ $this->equalTo($existingVersionPath) ]
             )
-            ->willReturnOnConsecutiveCalls(5, 10);
+            ->willReturnOnConsecutiveCalls(
+                $originalVersionAge,
+                $existingVersionAge
+            );
 
         $cache = new FileCache($fs);
-        $isInCache = $cache->isInCache($existingVersionPath, $originalVersionPath);
-        $this->assertTrue($isInCache, 'Should be in cache when existing version is newer than the original one');
+        return $cache->isInCache($existingVersionPath, $originalVersionPath);
     }
 
 }
