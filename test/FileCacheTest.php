dbab11fd0e4c824d9ba4aa7e64471d7c0042d405
Rubén Chavarría <txingo@gmail.com>
2016-03-22 09:01:46 +0100

    New test: non existent files are not in cache

--- test/FileCacheTest.php
+++ test/FileCacheTest.php
@@ -4,8 +4,12 @@ require_once 'FileCache.php';
 
 class FileCacheTest extends PHPUnit_Framework_TestCase {
 
-    public function testIsInstanciable() {
-      $this->assertNotNull(new FileCache());
+    public function testNonExistentFilesAreNotInCache() {
+        $fs = $this->getMockBuilder('FileSystem')->getMock();
+        $fs->method('file_exists')->willReturn(false);
+        $cache = new FileCache($fs);
+
+        $this->assertFalse($cache->isInCache(null, null));
     }
-    
+
 }
