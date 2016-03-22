61668439aa9b05fe362dcd08869e690a95062686
Rubén Chavarría <txingo@gmail.com>
2016-03-22 09:14:58 +0100

    New test: files which existing version is older than the original version are not considered as cached files

--- test/FileCacheTest.php
+++ test/FileCacheTest.php
@@ -12,4 +12,21 @@ class FileCacheTest extends PHPUnit_Framework_TestCase {
         $this->assertFalse($cache->isInCache(null, null));
     }
 
+    public function testFileIsNotInCacheWhenExistingVersionIsOlderThanOriginalFile() {
+        $existingVersionPath = 'existing-version-path.jpg';
+        $originalVersionPath = 'original-version-path.jpg';
+
+        $fs = $this->getMockBuilder('FileSystem')->getMock();
+        $fs->method('file_exists')->willReturn(true);
+        $fs->method('lastModificationDate')
+            ->withConsecutive(
+                [ $this->equalTo($originalVersionPath) ],
+                [ $this->equalTo($existingVersionPath) ]
+            )
+            ->willReturnOnConsecutiveCalls(10, 5);
+
+        $cache = new FileCache($fs);
+        $this->assertFalse($cache->isInCache($existingVersionPath, $originalVersionPath));
+    }
+
 }
