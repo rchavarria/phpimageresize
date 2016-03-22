72441c3fbae519cb3e615f0dfe80ca0a4aae487f
Rubén Chavarría <txingo@gmail.com>
2016-03-22 08:52:00 +0100

    Copy isInCache() method to FileCache class

--- FileCache.php
+++ FileCache.php
@@ -2,4 +2,20 @@
 
 class FileCache {
 
+    function isInCache($path, $imagePath) {
+        $fs = new FileSystem();
+
+        $isInCache = false;
+        if(file_exists($path) == true):
+            $isInCache = true;
+            $origFileTime = $fs->lastModificationDate($imagePath);
+            $newFileTime = $fs->lastModificationDate($path);
+            if($newFileTime < $origFileTime): # Not using $opts['expire-time'] ??
+                $isInCache = false;
+            endif;
+        endif;
+
+        return $isInCache;
+    }
+
 }
