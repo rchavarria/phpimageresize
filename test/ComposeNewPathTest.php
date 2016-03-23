01ef8c8e1d725522142ce3f70ef74722e41ab063
Rubén Chavarría <txingo@gmail.com>
2016-03-23 08:52:49 +0100

    New test: includes cache path from configuration options

--- test/ComposeNewPathTest.php
+++ test/ComposeNewPathTest.php
@@ -5,11 +5,21 @@ class ComposeNewPathTest extends PHPUnit_Framework_TestCase {
     public function testCanBeConfiguredWithAnOption() {
         $fs = $this->getMock('FileSystem');
         $expectedPath = 'new-path-to-the-heaven.php';
-        $options = [ 'output-filename' => $expectedPath ];
+        $options = [ Configuration::OUTPUT_FILENAME_KEY => $expectedPath ];
 
         $configuration = new Configuration($options, $fs);
-        $newPath = $configuration->composeNewPath('some-other-file-path.php');
+        $newPath = $configuration->composeNewPath('file-path.php');
         $this->assertEquals($expectedPath, $newPath);
+    }
+
+    public function testIncludesConfiguredCachePath() {
+        $fs = $this->getMock('FileSystem');
+        $cachePath = 'cache-path';
+        $options = [ Configuration::CACHE_KEY => $cachePath ];
+
+        $configuration = new Configuration($options, $fs);
+        $newPath = $configuration->composeNewPath('file-path.php');
+        $this->assertContains($cachePath, $newPath);
     }
 
 }
