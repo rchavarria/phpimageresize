46051b92225047a8a88f13f5fc202db1d5a7a3af
Rubén Chavarría <txingo@gmail.com>
2016-03-23 08:21:26 +0100

    Fix: return value

--- Resizer.php
+++ Resizer.php
@@ -60,7 +60,7 @@ class Resizer {
 
     private function fileNotExpired($filePath) {
         $cacheMinutes = $this->configuration->obtainCacheMinutes();
-        $this->fileSystem->filemtime($filePath) < strtotime('+'. $cacheMinutes. ' minutes');
+        return $this->fileSystem->filemtime($filePath) < strtotime('+'. $cacheMinutes. ' minutes');
     }
 
     private function checkPath($path) {
